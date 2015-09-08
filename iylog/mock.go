6202aed10f4f0a207bdcbe8365a9a751c4f67eed
Edd Robinson <me@edd.io>
2015-09-08 18:13:36 +0100

    MockLogger is not really a mock

--- iylog/mock.go
+++ iylog/mock.go
@@ -6,14 +6,14 @@ import (
 	"sync"
 )
 
-// MockLogger is a mock implementation of a Logger.
+// MemLogger is an in-memory implementation of a Logger.
 //
-// A MockLogger can be used to check if log calls were made. By default
-// a MockLogger will capture any logged messages at level DEBUG or
+// A MemLogger can be used to check if log calls were made. By default
+// a MemLogger will capture any logged messages at level DEBUG or
 // above. This can be changed using the SetLevel method.
 //
-// A MockLogger is safe for use by multiple goroutines.
-type MockLogger struct {
+// A MemLogger is safe for use by multiple goroutines.
+type MemLogger struct {
 	i int
 
 	mu       sync.Mutex
@@ -30,57 +30,57 @@ func (m Message) String() string {
 	return fmt.Sprintf(m.Format, m.Args...)
 }
 
-func NewMockLogger() *MockLogger {
-	return &MockLogger{level: DEBUG}
+func NewMemLogger() *MemLogger {
+	return &MemLogger{level: DEBUG}
 }
 
-func (l *MockLogger) Reset() {
+func (l *MemLogger) Reset() {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 	l.messages, l.i = []Message{}, 0
 }
 
-func (l *MockLogger) Printf(format string, v ...interface{}) {
+func (l *MemLogger) Printf(format string, v ...interface{}) {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 	l.messages = append(l.messages, Message{Format: format, Args: v})
 }
 
-func (l *MockLogger) Level() Level {
+func (l *MemLogger) Level() Level {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 	return l.level
 }
 
-func (l *MockLogger) Messages() []Message {
+func (l *MemLogger) Messages() []Message {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 	return l.messages
 }
 
-func (l *MockLogger) SetLevel(level Level) {
+func (l *MemLogger) SetLevel(level Level) {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 	l.level = level
 }
 
-func (l *MockLogger) Called() bool {
+func (l *MemLogger) Called() bool {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 	return len(l.messages) > 0
 }
 
-func (l *MockLogger) CalledN() int {
+func (l *MemLogger) CalledN() int {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 	return len(l.messages)
 }
 
-func (l *MockLogger) CalledWith(format string, v ...interface{}) bool {
+func (l *MemLogger) CalledWith(format string, v ...interface{}) bool {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 	if len(l.messages) == 0 {
-		panic("MockLogger was never called")
+		panic("MemLogger was never called")
 	}
 	for _, msg := range l.messages {
 		result := format == msg.Format && reflect.DeepEqual(v, msg.Args)
@@ -91,7 +91,7 @@ func (l *MockLogger) CalledWith(format string, v ...interface{}) bool {
 	return false
 }
 
-func (l *MockLogger) NextCalledWith(format string, v ...interface{}) bool {
+func (l *MemLogger) NextCalledWith(format string, v ...interface{}) bool {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 	if len(l.messages) <= l.i {
@@ -102,11 +102,11 @@ func (l *MockLogger) NextCalledWith(format string, v ...interface{}) bool {
 	return result
 }
 
-func (l *MockLogger) LastCalledWith(format string, v ...interface{}) bool {
+func (l *MemLogger) LastCalledWith(format string, v ...interface{}) bool {
 	l.mu.Lock()
 	defer l.mu.Unlock()
 	if len(l.messages) == 0 {
-		panic("MockLogger was never called")
+		panic("MemLogger was never called")
 	}
 	return format == l.messages[len(l.messages)-1].Format && reflect.DeepEqual(v, l.messages[len(l.messages)-1].Args)
 }
